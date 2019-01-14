a71376ad27a076f6bd9083e5f31754186faedf28
lvtong <lv199881@gmail.com>
2019-01-15 01:50:18 +0800

    第六章

---
+++ app/src/main/java/com/example/a22939/geoquiz6/Question.java
@@ -0,0 +1,39 @@
+package com.example.a22939.geoquiz6;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    private boolean isCheat;
+
+    public Question(int textResId, boolean answerTrue, boolean ischeat) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+        isCheat = ischeat;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public boolean isCheat() {
+        return isCheat;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public void setCheat(boolean cheat) {
+        isCheat = cheat;
+    }
+
+}
